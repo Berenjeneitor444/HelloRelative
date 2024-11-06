7073a2432da4ea5cded6462923c5d500fea89873
Berenjeneitor444 <dciriano2@gmail.com>
2024-11-06 15:50:44 +0100

    relative layout finished

---
+++ app/src/main/java/com/example/hellorelative/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.hellorelative;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView numero;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        numero = findViewById(R.id.textView);
+        findViewById(R.id.count).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numero.setText(String.valueOf(Integer.parseInt(numero.getText().toString()) + 1));
+            }
+        });
+        findViewById(R.id.toast).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(MainActivity.this, "You have counted " + numero.getText() + " times", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+    }
+}
